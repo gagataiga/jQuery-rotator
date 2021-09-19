ffd3e87a6b849015a71ebb1ed1c5220cb83aad13
志賀大河 <gaitatg97@gmail.com>
2021-09-19 19:40:46 +0200

    p element rotator

--- rotator.js
+++ rotator.js
@@ -1,6 +1,32 @@
 (function(){
-	"use strict";
-	
-	
+	"use strict";	
+	const contents = $("#container p");
+	let counter = 1;
+	rotator();
+	function rotator() { 
+		$(`#container p:nth-child(${counter})`).fadeIn(2000, function () { 
+			//
+			if ($(this).is("#container p:last-child")) {
+				//
+				setTimeout(function () {
+					$(`#container p:nth-child(${counter})`).fadeOut(2000, function () {
+						counter = 1;
+						rotator();
+					});
+				}, 3000);
+					
+			} else {
+				// 
+				setTimeout(function () {
+					$(`#container p:nth-child(${counter})`).fadeOut(2000, function () {
+						counter++;
+						rotator()
+					});
+					// 7 seconds wait
+				}, 3000);
+			}
+
+		 });
+	}
 
 }());
